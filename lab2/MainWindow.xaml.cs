16766b3d89fc06d31359cded6745f2ba7e66a0aa
Михайло Приймак <karapus1109@gmail.com>
2023-10-26 12:43:51 +0300

    Добавьте файлы проекта.

---
+++ lab2/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int count_pen = Convert.ToInt16(pen.Text);
+            int count_feltpen = Convert.ToInt16(felt_pen.Text);
+
+            Pen p = new Pen(count_pen);
+
+            double result_pen1 = p.Count_price();
+
+            felttip_pen f = new felttip_pen(count_feltpen);
+
+            double result_feltpen1 = f.Count_price_felttip();
+
+            result_pen.Text = result_pen1.ToString();
+            result_feltpen.Text = result_feltpen1.ToString();
+        }
+    }
+}
---
+++ lab2/Pen.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    internal class Pen
+    {
+        protected double price;
+        protected int count;
+
+        public Pen(int count1)
+        {
+            price = 18.5;
+            count = count1;
+        }
+
+        public double Count_price()
+        {
+            double price_pen;
+
+            price_pen = price * count;
+
+            return price_pen;
+        }
+    }
+
+    internal class felttip_pen : Pen
+    {
+        protected double price_felt;
+        protected int count_felt;
+        private int sort;
+
+        public felttip_pen(int count1) : base(count1)
+        {
+            sort = 3;
+            count_felt = count1;
+
+            double formula = 1.0 / sort;
+
+            price_felt = (price / formula) ;
+            
+        }
+
+        public double Count_price_felttip()
+        {
+            double price_felts;
+
+            price_felts = price_felt * count_felt;
+
+            return price_felts;
+        }
+    }
+
+}
